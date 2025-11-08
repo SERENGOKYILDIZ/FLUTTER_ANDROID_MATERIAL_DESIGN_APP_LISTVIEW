3deed1ee0f002f75d40dfdfbda7bb4c9b941f78a
Semi Eren Gokyildiz <127510869+SERENGOKYILDIZ@users.noreply.github.com>
2025-11-08 17:02:58 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/android_flutter_material_design_app_listview/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.android_flutter_material_design_app_listview
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Anasayfa(),
+    );
+  }
+}
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrangeAccent,
+        title: Text("Yemek Uygulaması"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
